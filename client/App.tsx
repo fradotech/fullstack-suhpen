dc22ad34d386398cce9c18399979c2f25ed09dae
adyjul <adyjuliyanto2001@gmail.com>
2023-03-01 19:17:59 +0700

    fix size icon fold and unfold

--- client/App.tsx
+++ client/App.tsx
@@ -2,6 +2,7 @@ import { ConfigProvider } from 'antd'
 import React from 'react'
 import BrowserRouter from './BrowserRouter'
 import { globalThemeConfig } from './utils/theme'
+import './index.css'
 
 const App: React.FC = () => {
   return (
