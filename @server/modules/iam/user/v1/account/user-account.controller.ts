4ec3a2d14011ff35b3ce4f4e1a06dc254ac1aac4
fradotech <fradotech.id@gmail.com>
2023-03-13 12:35:51 +0700

    Add section sub module in v1 folder

--- @server/modules/iam/user/v1/account/user-account.controller.ts
+++ @server/modules/iam/user/v1/account/user-account.controller.ts
@@ -3,12 +3,12 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
 import { IApiRes } from '@server/infrastructure/interfaces/api-responses.interface'
 import { ApiRes } from '@server/infrastructure/interfaces/api.response'
 import { Modules } from '@server/modules/modules'
-import { LoggedInGuard } from '../../auth/common/logged-in.guard'
-import { GetUserLogged } from '../common/get-user-logged.decorator'
-import { IUser } from '../infrastructure/user.interface'
-import { UserUpdateRequest } from '../infrastructure/user.request'
-import { UserResponse } from '../infrastructure/user.response'
-import { UserCrudApp } from './user-crud.app'
+import { LoggedInGuard } from '../../../auth/common/logged-in.guard'
+import { GetUserLogged } from '../../common/get-user-logged.decorator'
+import { IUser } from '../../infrastructure/user.interface'
+import { UserUpdateRequest } from '../../infrastructure/user.request'
+import { UserResponse } from '../../infrastructure/user.response'
+import { UserCrudApp } from '../user-crud.app'
 
 const THIS_MODULE = Modules.Account
 
