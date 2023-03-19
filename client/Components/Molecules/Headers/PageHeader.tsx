7cd3ea51ae848e1795f2fe3e686ec714857cbf78
fradotech <fradotech.id@gmail.com>
2023-03-19 13:18:25 +0700

    Add detail action button edit and delete

--- client/Components/Molecules/Headers/PageHeader.tsx
+++ client/Components/Molecules/Headers/PageHeader.tsx
@@ -1,27 +1,45 @@
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
+import { Button, Popconfirm, Row } from 'antd'
 import Title from 'antd/es/typography/Title'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { axiosService } from '../../../services/axios.service'
 import Loading from '../Loading/Loading'
+import styles from './PageHeader.module.css'
 
 interface IProps {
   title: string
-  hrefCreate?: string
   isLoading?: boolean
+  hrefEdit?: string
+  hrefDelete?: string
 }
 
 export const PageHeader: React.FC<IProps> = (props: IProps) => {
   return (
     <>
       <Loading isLoading={props.isLoading} />
-      <Title
-        style={{
-          fontSize: '24px',
-          lineHeight: '32px',
-          marginTop: 0,
-          marginLeft: 2,
-        }}
-      >
-        {props.title}
-      </Title>
+      <Row className={styles.container}>
+        <Title className={styles.title}>{props.title}</Title>
+        <Row>
+          {props.hrefEdit && (
+            <Link to={props.hrefEdit}>
+              <Button type="primary" className={styles.actionButton}>
+                <EditOutlined />
+              </Button>
+            </Link>
+          )}
+          {props.hrefDelete && (
+            <Popconfirm
+              title={'Are you sure want to delete?'}
+              onConfirm={() => axiosService.delete(props.hrefDelete)}
+            >
+              <Button type="primary" className={styles.actionButton} danger>
+                <DeleteOutlined />
+              </Button>
+            </Popconfirm>
+          )}
+        </Row>
+      </Row>
     </>
   )
 }
