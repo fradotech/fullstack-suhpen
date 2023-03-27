4ebbd32d6919071fb5968142ca2705c8a11a73aa
fradotech <fradotech.id@gmail.com>
2023-03-27 22:21:10 +0700

    Detail products

--- client/Components/Organisms/Description/DescriptionItem.tsx
+++ client/Components/Organisms/Description/DescriptionItem.tsx
@@ -1,16 +1,12 @@
 import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons'
 import { IBaseEntity } from '@server/infrastructure/base/base-entity.interface'
-import { Descriptions, Image } from 'antd'
+import { Descriptions, Image, Tag } from 'antd'
 import React from 'react'
+import { themeColors } from '../../../utils/theme'
 import { Util } from '../../../utils/util'
 
 const DescriptionItem = (data: IBaseEntity, key: string) => {
-  if (
-    key == 'avatar' ||
-    key == 'image' ||
-    key == 'thumbnail' ||
-    key == 'attachment'
-  ) {
+  if (['avatar', 'image', 'thumbnail', 'attachment'].includes(key)) {
     return (
       <Descriptions.Item label={Util.titleCase(key)}>
         <Image style={{ width: '50px' }} src={data[key]} />
@@ -32,6 +28,24 @@ const DescriptionItem = (data: IBaseEntity, key: string) => {
         {Util.formatDatetime(data[key]) != 'Invalid Date'
           ? Util.formatDatetime(data[key])
           : '-'}
+      </Descriptions.Item>
+    )
+  } else if (data[key]?.id && data[key]?.name) {
+    return (
+      <Descriptions.Item label={Util.titleCase(key)}>
+        {data[key].name || '-'}
+      </Descriptions.Item>
+    )
+  } else if (Array.isArray(data[key])) {
+    return (
+      <Descriptions.Item label={Util.titleCase(key)}>
+        {data[key].map((data: IBaseEntity) => {
+          return (
+            <Tag color={data['labelColor'] || themeColors.primary}>
+              {data['name']}
+            </Tag>
+          )
+        })}
       </Descriptions.Item>
     )
   } else {
