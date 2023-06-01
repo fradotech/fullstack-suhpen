b7e2b01361b8696e8cd5583437df57953e6491d1
fradotech <fradotech.id@gmail.com>
2023-06-01 10:52:53 +0700

    Api service catch message

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import react from '@vitejs/plugin-react-swc'
-import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
-
-dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 3001 },
-  define: {
-    __VALUE__: `"${process.env.VALUE}"`, // wrapping in "" since it's a string
-  },
 })
