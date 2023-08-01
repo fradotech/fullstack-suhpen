40375bf0ec2d742e5bdb2e40c3daefff6dce83c6
fradotech <fradotech.id@gmail.com>
2023-08-01 08:58:49 +0700

    perf: remove injectable sentry module

--- @server/database/seeder/seeder.module.ts
+++ @server/database/seeder/seeder.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Logger } from '@nestjs/common'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { permissionSyncSeeder } from '@server/modules/iam/permission/database/permission-sync.seeder'
 import { roleCreateSeeder } from '@server/modules/iam/role/database/role-create.seeder'
@@ -10,7 +10,6 @@ import { EntityManager } from 'typeorm'
 import { userCreateSeeder } from '../../modules/iam/user/database/user-create.seeder'
 import dataSource from '../data-source'
 
-@Injectable()
 export class SeederMoodule {
   static async forRoot(app: NestExpressApplication) {
     await dataSource
