d2393d98f8af9679a0ded09008f41fbbff533ccf
fradotech <fradotech.id@gmail.com>
2023-09-29 22:32:35 +0700

    fix: default database postgres

--- @server/database/common/database.config.ts
+++ @server/database/common/database.config.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const DATABASE_CONFIG = {
-  dialect: process.env.DB_SERVER || 'mysql',
+  dialect: process.env.DB_SERVER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USERNAME || 'root',
+  port: process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE || fradotech,
 }
