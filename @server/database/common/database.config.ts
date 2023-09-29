88decf8f35b5b7252ba03211c2f3676d37032a72
fradotech <fradotech.id@gmail.com>
2023-09-29 22:33:12 +0700

    fix: default database postgres

--- @server/database/common/database.config.ts
+++ @server/database/common/database.config.ts
@@ -8,6 +8,6 @@ export const DATABASE_CONFIG = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || 'frado201',
   database: process.env.DB_DATABASE || fradotech,
 }
